1338ad807a1119c9309cfb1efab844380c6c821c
Malinga Bandara <65806631+MalingaBandara@users.noreply.github.com>
2021-07-19 18:27:26 +0530

    POS

---
+++ Pos/src/pos/Pos.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pos;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+/**
+ *
+ * @author malinga
+ */
+public class Pos {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static String connection ="jdbc:mysql://localhost:3306/NSBM_POS";
+    public static String Host= "root";
+    public static String Password="spymali1021";
+
+    
+    public static void main(String[] args) {
+         
+         
+            
+        
+            
+    }
+    
+ 
+    
+    
+}
